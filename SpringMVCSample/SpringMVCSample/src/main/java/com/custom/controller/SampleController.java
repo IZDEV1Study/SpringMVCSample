62fb23f8c5fa0bbbeb21c9e07aa8cb364a42d83c
YalySeung <yalyseung@gmail.com>
2022-10-30 21:22:22 +0900

    servlet API 적용

--- SpringMVCSample/SpringMVCSample/src/main/java/com/custom/controller/SampleController.java
+++ SpringMVCSample/SpringMVCSample/src/main/java/com/custom/controller/SampleController.java
@@ -1,13 +1,17 @@
 package com.custom.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
-@Controller
-public class SampleController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String homePage(){
-        return "index";
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SampleController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("index");
+
+        return mv;
     }
 }
