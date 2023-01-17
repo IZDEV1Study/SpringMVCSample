71d7d238566e2931398a9aef257cd0d1aa46fa8d
shyang <shyang@inzisoft.com>
2023-01-17 17:21:47 +0900

    폴더구조 변경

--- jdbc-sample/src/test/java/TestSample.java
+++ jdbc-sample/src/test/java/TestSample.java
@@ -8,13 +8,13 @@ import java.util.logging.Logger;
 public class TestSample {
     public static Logger log = Logger.getLogger("DepartmentMain");
 
-    @Test
-    public void testConnet() {
-        DepartmentDAO dao = new DepartmentDAO();
-        List<Department> list = dao.selectList();
-
-        print(list);
-    }
+//    @Test
+//    public void testConnet() {
+//        DepartmentDAO dao = new DepartmentDAO();
+//        List<Department> list = dao.selectList();
+//
+//        print(list);
+//    }
 
     void print(List<Department> list) {
         for (Department department : list) {
